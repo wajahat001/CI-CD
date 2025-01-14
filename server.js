7a01668c9c894990611b8ea20131cff3bcc39724
wajahat001 <wajahatchaudy1640@gmail.com>
2025-01-14 20:11:08 +0500

    updates

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server on port 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
